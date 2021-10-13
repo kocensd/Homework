9ff08c0f5c33b2e29f7be1782565ee01c291465f
SangDo <koreacenterappteamsd@gmail.com>
2021-10-13 09:18:09 +0900

    - test

--- Homework/AppDelegate.swift
+++ Homework/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         viewController.reactor = ViewControllerReactor(service: Service())
         
         return true
+        
     }
 }
 
